aa2285ce78a7f1b253d773698e47064b085e1a19
moling <365024424@qq.com>
2019-12-28 13:50:51 +0800

    修复Api的bug

--- src/api/index.js
+++ src/api/index.js
@@ -2,12 +2,12 @@ import request from '@/common/request'
 
 const api = {
   login: params => request('POST', '/login', params), // 用户登录
-  getUserInfo: request('GET', '/user/info'), // 获取登录用户
-  getImageCode: request('GET', '/imagecode'), // 获取图片验证码
+  getUserInfo: _ => request('GET', '/user/info'), // 获取登录用户
+  getImageCode: _ => request('GET', '/imagecode'), // 获取图片验证码
   sendSmsCode: params => request('POST', '/sendsmscode', params), // 发送短信验证码
   checkAccount: params => request('POST', '/checkaccount', params), // 检查帐号的合法性
   register: params => request('POST', '/register', params), // 注册帐号
-  logout: request('POST', '/user/logout'), // 退出登录
+  logout: _ => request('POST', '/user/logout'), // 退出登录
   getNews: params => request('POST', '/news', params), // 查询文章列表
   getNewsById: id => request('GET', `/news/${id}`) // 查询文章详情
 }
