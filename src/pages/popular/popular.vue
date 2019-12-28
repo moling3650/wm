22bad984edff3524240e2fbc5b585ccf8009ff53
moling <365024424@qq.com>
2019-12-28 16:47:03 +0800

    用组件重构推荐页面和热门页面

--- src/pages/popular/popular.vue
+++ src/pages/popular/popular.vue
@@ -1,147 +1,38 @@
 <template>
-  <view>
-    <view class="recomm-container list">
-      <view v-for="(item,index) in newList" :key="index" class="list-item">
-        <list-view :options="item" @click="gotoDetail(item)" @someLove="someLove"></list-view>
-      </view>
-    </view>
-    <view class="loading-text">
-      {{contentext}}
-    </view>
+  <view id="popular">
+    <news-list ref="news" meta-id="1906032244030001" />
   </view>
 </template>
 
 <script>
-  import listView from '@/components/listview.vue'
+  import newsList from '@/components/newsList'
 
-  let _self, _pageIndex = 1,
-    timer = null;
   export default {
-    name: 'recommend',
+    name: 'popular',
     components: {
-      listView
+      newsList
     },
-    data() {
-      return {
-        newList: [],
-        pageSize: 20,
-        contentext: ''
-      }
+    // 监听页面初次渲染完成
+    onReady() {
+      this.$refs.news.getNews()
     },
-    onLoad() {
-      // this._getMeta()
-      this._getNews();
-
-      // uni.startPullDownRefresh();
+    // 监听用户下拉动作
+    onPullDownRefresh() {
+      this.$refs.news.getNews()
     },
-    methods: {
-      onPullDownRefresh() {
-        // 下拉刷新请求第一次数据
-        this._getNews()
-      },
-      onReachBottom() {
-        if (timer != null) {
-          clearTimeout(timer)
-        }
-        this.timer = setTimeout(() => {
-          this.contentext = '加载中。。。'
-          this.getMoreNews();
-        }, 1000)
-      },
-      _getMeta() {
-        this.$api.meta({}).then(res => {
-          console.log(res, "ddd")
-        })
-      },
-      getMoreNews() {
-        uni.showNavigationBarLoading() // 显示加载动画
-        const parmes = {
-          pageIndex: _pageIndex,
-          pageSize: this.pageSize,
-          metaId: '1906032244030001'
-        }
-        this.$api.news(parmes).then(res => {
-          const {
-            code,
-            data
-          } = res || {}
-          if (data == null) {
-            uni.hideNavigationBarLoading()
-            return
-          }
-          if (code === 200) {
-            _pageIndex++
-            this.newList = this.newList.concat(data.items)
-            uni.hideNavigationBarLoading()
-          }
-        })
-      },
-      _getNews() {
-        _pageIndex = 1;
-        uni.showNavigationBarLoading()
-        const parmes = {
-          pageIndex: _pageIndex,
-          pageSize: this.pageSize,
-          metaId: '1906032244030001'
-        }
-        this.$api.news(parmes).then(res => {
-          _pageIndex++;
-          const {
-            code,
-            data
-          } = res || {}
-          if (code === 200) {
-            this.newList = data.items
-            uni.hideNavigationBarLoading()
-            uni.stopPullDownRefresh()
-          }
-        })
-      },
-      someLove(newList) {
-        const parmes = {
-          id: newList.id,
-          targetId: newList.targetId,
-        }
-        let minus = -1;
-        this.$api.parise(parmes).then(res => {
-          newList.status = minus
-          console.log(res, '----------------')
-        })
-        console.log(newList, 'newList')
-      },
-      gotoDetail(detail) {
-        uni.navigateTo({
-          url: '/pages/detail/detail?query=' + detail.id
-        });
-        console.log('跳转详情')
-      }
+    // 页面滚动到底部的事件
+    onReachBottom() {
+      this.$refs.news.getMoreNews()
     }
   }
 </script>
 
-<style lang="scss" scoped>
+<style lang="css" scoped>
   /* #ifndef APP-PLUS */
   page {
     width: 100%;
     min-height: 100%;
     display: flex;
   }
-
   /* #endif */
-  .list {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    // padding: 29rpx;
-  }
-
-  .list-item {
-    margin: 20rpx 0 0 20rpx;
-  }
-
-  .loading-text {
-    width: 100%;
-    text-align: center;
-    color: #2F85FC;
-  }
 </style>
--- src/pages/recommend/recommend.vue
+++ src/pages/recommend/recommend.vue
@@ -1,119 +1,28 @@
 <template>
-  <view>
-    <view class="recomm-container list">
-      <view v-for="(item, index) in newList" :key="index" class="list-item">
-        <list-view :options="item" @click="gotoDetail(item)" @someLove="someLove"></list-view>
-      </view>
-    </view>
-    <view class="loading-text">
-      {{ contentext }}
-    </view>
+  <view id="recommend">
+    <news-list ref="news" meta-id="1906032244030001" />
   </view>
 </template>
 
 <script>
-  import listView from '@/components/listview.vue'
+  import newsList from '@/components/newsList'
 
   export default {
     name: 'recommend',
     components: {
-      listView
+      newsList
     },
-    data() {
-      return {
-        newList: [],
-        pageIndex: 1,
-        pageSize: 20,
-        contentext: '',
-        metaId: '1906032244030001',
-        timer: null
-      }
+    // 监听页面初次渲染完成
+    onReady() {
+      this.$refs.news.getNews()
     },
-    onLoad() {
-      // this._getMeta()
-      this._getNews();
+    // 监听用户下拉动作
+    onPullDownRefresh() {
+      this.$refs.news.getNews()
     },
-    methods: {
-      onPullDownRefresh() {
-        // 下拉刷新请求第一次数据
-        this._getNews()
-      },
-      onReachBottom() {
-        if (this.timer != null) {
-          clearTimeout(this.timer)
-        }
-        this.timer = setTimeout(() => {
-          this.contentext = '加载中。。。'
-          this.getMoreNews();
-        }, 1000)
-      },
-      _getMeta() {
-        this.$api.meta({}).then(res => {
-          console.log(res, "ddd")
-        })
-      },
-      getMoreNews() {
-        uni.showNavigationBarLoading() // 显示加载动画
-        const parmes = {
-          pageIndex: this.pageIndex,
-          pageSize: this.pageSize,
-          metaId: this.metaId
-        }
-        this.$api.news(parmes).then(res => {
-          const {
-            code,
-            data
-          } = res || {}
-          if (data == null) {
-            uni.hideNavigationBarLoading()
-            return
-          }
-          if (code === 200) {
-            this.pageIndex++
-            this.newList = this.newList.concat(data.items)
-            uni.hideNavigationBarLoading()
-          }
-        })
-      },
-      _getNews() {
-        this.pageIndex = 1;
-        uni.showNavigationBarLoading()
-        const parmes = {
-          pageIndex: this.pageIndex,
-          pageSize: this.pageSize,
-          metaId: this.metaId
-        }
-        this.$api.news(parmes).then(res => {
-          this.pageIndex++;
-          const {
-            code,
-            data
-          } = res || {}
-          if (code === 200) {
-            this.newList = data.items
-            uni.hideNavigationBarLoading()
-            uni.stopPullDownRefresh()
-          }
-        })
-      },
-      someLove(newList) {
-        const parmes = {
-          id: newList.id,
-          targetId: newList.targetId,
-        }
-        let minus = -1;
-        this.$api.parise(parmes).then(res => {
-          newList.status = minus
-          console.log(res, '----------------')
-        })
-        console.log(newList, 'newList')
-      },
-      gotoDetail(detail) {
-        uni.navigateTo({
-          url: '/pages/detail/detail?query=' + detail.id
-        });
-        console.log('跳转详情')
-      }
+    // 页面滚动到底部的事件
+    onReachBottom() {
+      this.$refs.news.getMoreNews()
     }
   }
 </script>
@@ -125,25 +34,5 @@
     min-height: 100%;
     display: flex;
   }
-
   /* #endif */
-  .list {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    // padding: 29upx;
-  }
-
-  .list-item {
-    margin: 20upx 0upx 0 20upx;
-    // &:nth-child(2n){
-    // 	margin-left: 0;
-    // }
-  }
-
-  .loading-text {
-    width: 100%;
-    text-align: center;
-    color: #2F85FC;
-  }
 </style>
