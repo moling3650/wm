48a46bb7a1d338a46f203bfff7794e02c380a435
moling <365024424@qq.com>
2020-01-18 15:47:26 +0800

    引入vuex

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App'
 import api from '@/api/index'
+import store from './store/index'
 
 Vue.config.productionTip = false
 Vue.prototype.$api = api
 App.mpType = 'app'
 
 const app = new Vue({
+  store,
   ...App
 })
 app.$mount()
